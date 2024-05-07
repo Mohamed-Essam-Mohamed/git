90fe06bf70d55650e92320a971d53c8d15e8379b
mohamed <esammww.com@gmail.com>
2024-05-07 19:48:27 +0300

    init app git

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(GitApp());
+}
+
+class GitApp extends StatelessWidget {
+  GitApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Git Test'),
+      ),
+      body: Column(
+        children: [
+          Text('Home Screen added splash screen'),
+        ],
+      ),
+    );
+  }
+}
