f3b3e49c59eea2030f6bb0d5a3d02724cf2af256
mohamed <esammww.com@gmail.com>
2024-06-03 23:58:30 +0300

    added theme

---
+++ lib/src/utils/app_theme.dart
@@ -0,0 +1,23 @@
+class AppTheme {
+  static const String lightTheme = 'light';
+  static const String darkTheme = 'dark';
+
+  static const String defaultTheme = lightTheme;
+
+  static const List<String> themes = [lightTheme, darkTheme];
+
+  static const Map<String, Map<String, dynamic>> themeData = {
+    // lightTheme: {
+    //   'appBarTheme': const AppBarTheme(
+    //     backgroundColor: Colors.white,
+    //     elevation: 0,
+    //   ),
+    // },
+    // darkTheme: {
+    //   'appBarTheme': const AppBarTheme(
+    //     backgroundColor: Colors.black,
+    //     elevation: 0,
+    //   ),
+    // },
+  };
+}
