83c388cc2cdc697f601c29c0c031d110a1159872
mohamed <esammww.com@gmail.com>
2024-06-03 23:55:43 +0300

    added text style

---
+++ lib/src/utils/app_text_style.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  static const TextStyle appTextStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+  );
+  static const TextStyle appTextStyle1 = TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+  );
+  static const TextStyle appTextStyle2 = TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+  );
+  static const TextStyle appTextStyle3 = TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+  );
+  static const TextStyle appTextStyle4 = TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+  );
+  static const TextStyle appTextStyle5 = TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+  );
+  static const TextStyle appTextStyle6 = TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+  );
+  static const TextStyle appTextStyle11 = TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+  );
+}
